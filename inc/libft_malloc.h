ce3526ee9fa33a5150e6b37deb48689e5748784a
Naerhy <naerhy@protonmail.com>
2023-07-18 21:20:49 +0200

    Define zones and blocks structures

--- inc/libft_malloc.h
+++ inc/libft_malloc.h
@@ -1,7 +1,22 @@
-#ifndef __LIBFT_MALLOC_H__
-#define __LIBFT_MALLOC_H__
+#ifndef LIBFT_MALLOC_H
+#define LIBFT_MALLOC_H
 
 #include <stdlib.h>
+
+typedef struct s_zone
+{
+	int type;
+	size_t size;
+	size_t free_size;
+	// nb blocks => maybe useless because to check if empty we can compare
+	// if size == free_size
+} t_zone;
+
+typedef struct s_block
+{
+	size_t size;
+	int is_free;
+} t_block;
 
 void* malloc(size_t size);
 void* realloc(void* ptr, size_t size);
